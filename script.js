61da3cde55a567e06e6a2fb1df8cb3ddd056c2ad
Camarederic <camarederic@gmail.com>
2026-03-03 23:23:18 +0300

    added more styles CSS

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-'use strict'
+"use strict";
 
+const menuIcon = document.querySelector(".menu-icon");
+const lines = document.querySelector(".lines");
+const container = document.querySelector(".container");
+
+menuIcon.addEventListener("click", () => {
+  container.classList.toggle("change");
+});
